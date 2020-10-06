4454af548ce9aa467122f0e06c9a2a13f1047ae5
Raphael Discky <zundria.putra@gmail.com>
2020-10-06 13:36:14 +0700

    Create SignIn screen UI

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/views/sign_in.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145c9e),
+        scaffoldBackgroundColor: Color(0xff1f1f1f),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignIn(),
+    );
+  }
+}
