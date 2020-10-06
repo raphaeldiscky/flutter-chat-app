9ad2a36c4ec2fdecfa5493a9f836039968c90d95
Raphael Discky <zundria.putra@gmail.com>
2020-10-06 22:13:30 +0700

    Create function to search the user by username with firestore query

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class DatabaseMethods {
-  getUserByUsername(String username) {}
+  getUserByUsername(String username) {
+    FirebaseFirestore.instance.collection("users").where("name", isEqualTo: username).get();
+  }
+
   uploadUserInfo(userMap) {
     FirebaseFirestore.instance.collection("users").add(userMap).catchError((e) {
       print(e.toString());
