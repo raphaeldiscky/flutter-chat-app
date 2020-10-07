df454cdc0f9eca0838910f1a03abdd695e632f3a
Raphael Discky <zundria.putra@gmail.com>
2020-10-07 18:54:14 +0700

    Create conversation screen UI

--- lib/views/conversation_screen.dart
+++ lib/views/conversation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/widgets/widget.dart';
 
 class ConversationScreen extends StatefulWidget {
   @override
@@ -6,8 +7,62 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  Widget chatMessageList() {}
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Color(0x54ffffff),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        //controller: searchTextEditingController,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "Message...",
+                          hintStyle: TextStyle(
+                            color: Colors.white54,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        //initiateSearch();
+                      },
+                      child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  const Color(0x36ffffff),
+                                  const Color(0x0fffffff),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(40)),
+                          padding: EdgeInsets.all(10),
+                          child: Image.asset("assets/images/send.png")),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
